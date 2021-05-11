bb7cd267d4b629ace74d1a701d95edef01d181c8
gitea <info@idcos.com>
2021-05-11 11:52:08 +0800

    更新 'run.sh'

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@ cd ./ansible
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_TF_DIR=..
 export ANSIBLE_NOCOWS=1
-sleep 30
 chmod +x terraform.py
 ansible-playbook -i ./terraform.py playbook.yml
