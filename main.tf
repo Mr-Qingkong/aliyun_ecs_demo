924f1dc3c5ba80f045f2536926fd9e531e35918f
徐晓航 <evan@MacBook-Pro-10.local>
2021-09-23 17:14:32 +0800

    init commit

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    ansible = {
-      source = "nbering/ansible"
-      version = "1.0.4"
-    }
-   // alicloud = {
-   //   source = "aliyun/alicloud"
-  //}
-  }
-}
-
-provider "alicloud" {}
-
-provider "ansible" {}
-
 resource "alicloud_vpc" "vpc" {
   vpc_name       = "tf_test_foo"
   cidr_block = "172.16.0.0/12"
@@ -38,11 +22,9 @@ resource "alicloud_instance" "instance" {
   instance_type        = var.instance_type
   system_disk_category = "cloud_efficiency"
   image_id             = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
-  count                = var.instance_number
   instance_name        = var.instance_name
   vswitch_id = alicloud_vswitch.vsw.id
   internet_max_bandwidth_out = 1
-  user_data = data.cloudinit_config.cloudiac.rendered
 
 }
 
@@ -57,12 +39,7 @@ resource "alicloud_security_group_rule" "allow_all_tcp" {
   cidr_ip           = "0.0.0.0/0"
 }
 
-resource "ansible_host" "web" {
-  count                = var.instance_number
-  inventory_hostname = alicloud_instance.instance[count.index].public_ip
-  groups             = ["web"]
-  vars = {
-      port = 80
-      ansible_user = "root"
-    }
+
+output "public_ip" {
+   value = alicloud_instance.instance.public_ip
 }
--- versions.tf
+++ versions.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     ansible = {
       source = "nbering/ansible"
-      version = "1.0.4"
-    }
+   }
     alicloud = {
       source = "aliyun/alicloud"
-      version = "1.124.3"
    }
   }
 }
